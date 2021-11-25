0d73949827d278ecfa83bac1f7465adac5a7d2c5
mxcl <mxcl@me.com>
2021-11-25 08:48:00 -0500

    Fix forecast to be the “day”-ish one

--- engine.ts
+++ engine.ts
@@ -1,4 +1,5 @@
 import { debug } from '@actions/core'
+import moment from 'moment'
 import fetch from 'node-fetch'
 
 export async function getForecast(lat: string | number, lng: string | number): Promise<string | undefined> {
@@ -11,15 +12,17 @@ export async function getForecast(lat: string | number, lng: string | number): P
     },
     status?: number
   }
-
   interface Rsp2 {
     properties: {
-      periods: {
-        number: number
-        detailedForecast: string
-      }[]
+      periods: Period[]
     },
     status: number
+  }
+  interface Period {
+    number: number
+    startTime: string
+    endTime: string
+    detailedForecast: string
   }
 
   const rsp1 = await fetch(`https://api.weather.gov/points/${lat},${lng}`).then(x => x.json()) as Rsp1
@@ -36,5 +39,14 @@ export async function getForecast(lat: string | number, lng: string | number): P
 
   debug(`out: ${JSON.stringify(rsp2, null, 2)}`)
 
-  return rsp2.properties.periods.find(x => x.number == 1)?.detailedForecast
+  const noon = moment().startOf('day').hour(12)
+  const day = (period: Period) => {
+    const start = moment(period.startTime)
+    const end = moment(period.endTime)
+    if (start.isBefore(noon) && end.isAfter(noon)) {
+      return period
+    }
+  }
+
+  return rsp2.properties.periods.find(day)?.detailedForecast
 }
